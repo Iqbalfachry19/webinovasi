4bc9a8168a2562508a83687a3b85e68823c001a8
iqbal <ifachry@rocketmail.com>
2024-08-06 10:23:09 +0700

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+function Navbar() {
+  return (
+    <div className="mx-auto flex max-w-[1000px] items-center justify-between p-4">
+      <div className="flex items-center">
+        <Image width={300} height={50} src="/Logo-Inovasi-Web.svg" alt="Logo" />
+      </div>
+      <nav className="flex space-x-4">
+        <p className="cursor-pointer">Home</p>
+        <p className="cursor-pointer">Tema</p>
+        <p className="cursor-pointer">Cara Custom Web</p>
+      </nav>
+      <button className="rounded bg-[#5B59C2] px-4 py-2 text-white hover:bg-blue-700">
+        Kontak Kami
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
