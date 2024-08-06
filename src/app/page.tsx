97dad70060fa49d1f1bf2963f808b7edaa4667ae
iqbal <ifachry@rocketmail.com>
2024-08-06 12:51:15 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import WebsiteTypes from "./components/WebisteTypes";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-
       <Hero />
+      <WebsiteTypes />
     </main>
   );
 }
