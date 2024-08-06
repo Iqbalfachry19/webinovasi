86c02781fa60153b7b58f50177654d377f240bd9
iqbal <ifachry@rocketmail.com>
2024-08-06 12:03:07 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Suspense>
-        <Hero />
-      </Suspense>
+
+      <Hero />
     </main>
   );
 }
