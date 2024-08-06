cb71309667450fe3fa14f352735a85139a0359e8
iqbal <ifachry@rocketmail.com>
2024-08-06 22:17:14 +0700

    fix

--- src/app/components/WebisteBuilder.tsx
+++ src/app/components/WebisteBuilder.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function WebsiteBuilder() {
   return (
     <div className="mx-auto flex max-w-[1150px] flex-col px-2 py-8 md:flex-row xl:flex-row">
-      <div className="relative h-[620px] w-full rounded-xl bg-cover md:ml-24 md:h-[380px] md:w-[300px] xl:h-[600px] xl:w-full xl:bg-contain">
+      <div className="relative h-[620px] w-full rounded-xl bg-cover md:ml-24 md:h-[380px] md:w-[290px] xl:h-[600px] xl:w-full xl:bg-contain">
         <Image src="/gambar-2.webp" alt="Business People" fill />
       </div>
 
