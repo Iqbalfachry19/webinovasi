aff02f689094c2b1c32035c2b45008d19909eb7c
iqbal <ifachry@rocketmail.com>
2024-08-06 11:14:41 +0700

    fix

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,47 +1,51 @@
 import React from "react";
-import Image from "next/image";
 
 function Hero() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      {/* Main Container */}
-      <div className="flex flex-col items-center bg-white p-8 text-center">
-        {/* Heading Section */}
-        <div className="mb-8">
-          <h1 className="mb-4 text-4xl font-bold">PERTAMA DI PEKANBARU</h1>
-          <h2 className="text-2xl font-semibold">
-            BIKIN WEBSITE
-            <br />
-            BISNISMU SENDIRI
-            <br />
-            TANPA PERLU
-            <br />
-            KODING
-          </h2>
-        </div>
+    <div className="mx-auto flex min-h-screen max-w-[1150px] flex-col items-center bg-center">
+      <div className="relative flex h-[600px] w-full flex-col items-center overflow-hidden rounded-2xl bg-black/20 bg-opacity-80 bg-[url('/Group-of-business-people-working-in-office.svg')] bg-cover p-8 text-center text-white bg-blend-overlay">
+        {/* Background Image */}
+        <div className="absolute inset-0 bg-black/50 bg-opacity-50 bg-blend-overlay"></div>
 
-        {/* Button Section */}
-        <div className="flex gap-4">
-          <a
-            className="inline-block rounded bg-green-500 px-6 py-3 text-white hover:bg-green-600"
-            href="http://wa.me/+6282287169964"
-          >
-            Coba Sekarang
-          </a>
-          <a
-            className="inline-block flex items-center rounded bg-blue-500 px-6 py-3 text-white hover:bg-blue-600"
-            href="#alasan"
-          >
-            <svg
-              aria-hidden="true"
-              className="mr-2 h-5 w-5"
-              viewBox="0 0 448 512"
-              xmlns="http://www.w3.org/2000/svg"
+        {/* Content Section */}
+        <div className="relative z-10 flex h-full w-full flex-col items-center justify-center">
+          {/* Heading Section */}
+          <div className="mb-8">
+            <h1 className="mb-4 text-3xl font-bold">PERTAMA DI PEKANBARU</h1>
+            <h2 className="text-6xl font-semibold">
+              BIKIN WEBSITE
+              <br />
+              BISNISMU SENDIRI
+              <br />
+              TANPA PERLU
+              <br />
+              KODING
+            </h2>
+          </div>
+
+          {/* Button Section */}
+          <div className="flex gap-4">
+            <a
+              className="inline-block rounded bg-[#5B59C2] px-6 py-3 text-white hover:bg-[#5B59C2]"
+              href="http://wa.me/+6282287169964"
             >
-              <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
-            </svg>
-            Pelajari Lebih Lanjut
-          </a>
+              Coba Sekarang
+            </a>
+            <a
+              className="group inline-block flex items-center rounded-full bg-white px-6 py-3 text-black duration-150 hover:bg-[#5B59C2] hover:text-white"
+              href="#alasan"
+            >
+              Pelajari Lebih Lanjut
+              <svg
+                aria-hidden="true"
+                className="ml-2 h-5 w-5 fill-current text-black duration-150 group-hover:text-white"
+                viewBox="0 0 448 512"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Navbar() {
   return (
-    <div className="mx-auto flex max-w-[1000px] items-center justify-between p-4">
+    <div className="mx-auto flex max-w-[1150px] items-center justify-between py-4">
       <div className="flex items-center">
         <Image width={300} height={50} src="/Logo-Inovasi-Web.svg" alt="Logo" />
       </div>
@@ -12,7 +12,7 @@ function Navbar() {
         <p className="cursor-pointer">Tema</p>
         <p className="cursor-pointer">Cara Custom Web</p>
       </nav>
-      <button className="rounded bg-[#5B59C2] px-4 py-2 text-white hover:bg-blue-700">
+      <button className="rounded bg-[#5B59C2] px-4 py-2 text-white hover:bg-[#5B59C2]">
         Kontak Kami
       </button>
     </div>
