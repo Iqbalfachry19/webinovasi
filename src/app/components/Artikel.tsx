f08cd055b8571b9c61b81591d45493925f1dea4b
iqbal <ifachry@rocketmail.com>
2024-08-07 17:45:50 +0700

    hi

--- src/app/components/Artikel.tsx
+++ src/app/components/Artikel.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Artikel() {
-  return <div>Artikel Terbaru</div>;
+  return (
+    <div>
+      <p>PULUHAN OWNER TELAH MEMPERCAYAKAN BRAND MEREKA KEPADA KAMI</p>
+      <p>Artikel Terbaru</p>
+    </div>
+  );
 }
 
 export default Artikel;
