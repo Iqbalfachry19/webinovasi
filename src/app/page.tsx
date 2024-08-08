059b8c16a76c1ba3f77bd24dad1dc38ee11380b4
iqbal <ifachry@rocketmail.com>
2024-08-08 19:16:38 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import Artikel from "./components/Artikel";
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Hero />
       <WebsiteTypes />
       <WebsiteBuilderSection />
