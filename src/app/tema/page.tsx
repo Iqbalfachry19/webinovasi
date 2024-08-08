16fab40ed8f1ec44d5e9fcf4e4d4b0d077258abc
iqbal <ifachry@rocketmail.com>
2024-08-08 19:15:14 +0700

    fix

--- src/app/tema/page.tsx
+++ src/app/tema/page.tsx
@@ -3,7 +3,10 @@ import Tema from "../components/Tema";
 
 function page() {
   return (
-    <div>
+    <div className="mx-auto mt-24 max-w-[1120px] text-center">
+      <p className="text-center text-4xl">
+        Saatnya Bangun Website Untuk Bisnismu Dengan Berbagai Tema Yang Menarik
+      </p>
       <Tema />
     </div>
   );
